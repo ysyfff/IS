a7e90ec8c7b155ad5ea5c7f1645aa8fda123ea75
ysyong <1156546473@qq.com>
2013-09-18 16:40:29 +0800

    highlight hand when hover on left-hand and right-hand

--- static/js/docs_js.js
+++ static/js/docs_js.js
@@ -5,6 +5,7 @@ var IS = {
     },
 };
 
+//nav的事件委托
 (function($){
     $('#is-nav').delegate('li', 'click', function(){
         $.each($('#is-nav').find('li'), function(i, obj){
@@ -12,6 +13,18 @@ var IS = {
             if(obj.hasClass('active')) obj.removeClass('active');
         });
         $(this).addClass('active');
+    });
+})(jQuery);
+
+//hand的颜色改变
+(function($){
+    $.each([$('#left-hand'), $('#right-hand')], function(i, obj){
+        var obj = $(obj);
+        obj.mouseenter(function(){
+            obj.find('span').addClass('active-hand');
+        }).mouseleave(function(){
+            obj.find('span').removeClass('active-hand');
+        })
     });
 })(jQuery);
 
